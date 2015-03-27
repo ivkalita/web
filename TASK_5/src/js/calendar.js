193965221a13fe57a8f0f4eaffd5b1f7c1812567
kaduev13 <kaduev13@gmail.com>
2015-03-27 15:06:56 +1000

    TASK 5. Calendar not ready yet, simple .date() method

--- TASK_5/src/js/calendar.js
+++ TASK_5/src/js/calendar.js
@@ -1,24 +1,52 @@
-var $ = require('./nojquery.js');
+var $ = require('./nojquery.js'),
+	daysNames = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+	monthNames = [
+		'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+		'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
+	]
+;
 
 function Calendar(elem, options) {
 	this._builtInLayouts = {
 		months: {
 			itemByRow: 4,
-			itemByCol: 3
+			itemByCol: 3,
+			finalizer: undefined
 		},
 		days: {
 			itemByRow: 7,
-			itemByCol: 6
+			itemByCol: 7,
+			finalizer: function(x, y, elem) {
+				if (y !== 0) {
+					return;
+				}
+				if (daysNames.length > x && x >= 0) {
+					elem
+						.class('calendar-data-cell calendar-data-cell-title')
+						.text(daysNames[x]);
+				}
+			}
 		},
 		years: {
 			itemByRow: 5,
 			itemByCol: 4
 		}
 	}
+
+	this._normalizeDay = function(day) {
+		if (day == 0) {
+			day = 7;
+		}
+		return day - 1;
+	}
+
 	this.elem = elem;
 	this.view = {}
 	elem.data('calendar', this);
-	elem.data('calendar').renderView();
+	elem.data('calendar')
+		.renderView()
+		.date(new Date())
+	;
 }
 
 Calendar.prototype.renderView = function() {
@@ -37,7 +65,7 @@ Calendar.prototype.renderView = function() {
 								.click(this.inputClick.bind($calendar))
 						)
 						.append(
-							this.view.searchBtn = $('<button></button>')
+							this.view.searchBtn = $('<div></div>')
 								.class('calendar-search-btn')
 								.click(this.searchClick.bind($calendar))
 								.text('F')
@@ -81,36 +109,101 @@ Calendar.prototype.renderView = function() {
 				)
 		)
 	;
-	this.setLayout('months');
+	return this;
 }
 
 //Calendar internal actions
 
+Calendar.prototype.switchMonth = function(month, year) {
+	this.daysPageStarts = new Date();
+	var dps = this.daysPageStarts;
+	dps.setDate(1);
+	dps.setMonth(month);
+	dps.setFullYear(year);
+	while (this._normalizeDay(dps.getDay()) !== 0) {	
+		dps.setDate(dps.getDate() - 1);
+	}
+	this.daysPageEnds = new Date(dps);
+	var dpe = this.daysPageEnds,
+		items = this.view.items
+	;
+	for (var y = 1; y < this.itemByCol; y++) {
+		for (var x = 0; x < this.itemByRow; x++) {
+			items[y][x].text(dpe.getDate());
+			dpe.setDate(dpe.getDate() + 1);
+		}
+	}
+	console.log.info(month);
+	this.view.info.text(monthNames(month) + ' ' + year);
+	return this;
+}
+
+Calendar.prototype.getDayPosition = function(date) {
+	if (this.daysPageStarts === undefined || date < this.daysPageStarts || this.daysPageEnds === undefined || date > this.daysPageEnds) {
+		return {x: -1, y: -1};
+	}
+	var iterator = new Date(this.daysPageStarts);
+	for (var y = 1; y < this.itemByCol; y++) {
+		for (var x = 0; x < this.itemByRow; x++) {
+			if (iterator.getMonth() === date.getMonth() && iterator.getDate() === date.getDate()) {
+				return {x: x, y: y};
+			}
+			iterator.setDate(iterator.getDate() + 1);
+		}
+	}
+}
+
+Calendar.prototype.refreshView = function(layout, date) {
+	if (typeof(date) === 'undefined') {
+		date = this.curDate;
+	}
+	if (this.layout !== layout) {
+		this.setLayout(layout);
+	}
+	if (layout === 'days') {
+		var dayPosition = this.getDayPosition(date);
+		if (dayPosition.x < 0 || dayPosition.y < 0) {
+			this.switchMonth(date.getMonth(), date.getFullYear());
+			dayPosition = this.getDayPosition(date);
+		}
+		this.view.items[dayPosition.y][dayPosition.x].text('sel');
+	}
+	return this;
+}
+
 Calendar.prototype.openIfNotOpened = function() {
 	if (!this.view.main.hasClass('calendar-fullsize')) {
 		this.view.main.toggleClass('calendar-fullsize');
-		// this.view.hideBtn.removeClass('invisible');
-	}	
+	}
+	return this;
 }
 
 Calendar.prototype.close = function() {
 	this.view.main.removeClass('calendar-fullsize');
+	return this;
 }
 
 Calendar.prototype.setLayout = function(layout) {
 	var itemByRow = 1,
-		itemByCol = 1
+		itemByCol = 1,
+		finalizer
 	;
 	if (this._builtInLayouts.hasOwnProperty(layout) >= 0) {
+		this.layout = layout;
 		itemByRow = this._builtInLayouts[layout].itemByRow;
 		itemByCol = this._builtInLayouts[layout].itemByCol;
+		finalizer = this._builtInLayouts[layout].finalizer;
+	} else {
+		this.layout = 'custom';
 	}
-	this.createPlaces(itemByCol, itemByRow);
+	this.itemByCol = itemByCol;
+	this.itemByRow = itemByRow;
+	return this.createPlaces(itemByCol, itemByRow, finalizer);
 }
 
 Calendar.prototype.createPlaces = function(itemByCol, itemByRow, finalize) {
 	if (itemByCol * itemByRow == 0) {
-		return;
+		return this;
 	}
 	var dataHeight = this.view.data.height(),
 		dataWidth = this.view.data.width(),
@@ -128,6 +221,7 @@ Calendar.prototype.createPlaces = function(itemByCol, itemByRow, finalize) {
 	);
 	for (var y = 0; y < itemByCol; y++) {
 		table.append(row = $('<tr></tr>'));
+		this.view.items.push([]);
 		for (var x = 0; x < itemByRow; x++) {
 			var item = $('<td></td>')
 				.class('calendar-data-cell')
@@ -137,10 +231,11 @@ Calendar.prototype.createPlaces = function(itemByCol, itemByRow, finalize) {
 			if (typeof(finalize) === 'function') {
 				finalize(x, y, item);
 			}
-			this.view.items[y * itemByRow + x] = item;
+			this.view.items[y][x] = item;
 			row.append(item);
 		}
 	}
+	return this;
 }
 
 //Calendar events
@@ -170,6 +265,30 @@ Calendar.prototype.nextClick = function() {
 
 Calendar.prototype.infoClick = function() {
 	console.log('info click');
+	return this;
+}
+
+//Calendar actions
+
+Calendar.prototype.date = function(value) {
+	var inDate;
+	if (typeof(value) === 'undefined') {
+		return this.curDate;
+	}
+	if (typeof(value) === 'string') {
+		inDate = new Date(value);
+		if (inDate === 'Invalid Date') {
+			console.log('Warn: Calendar.prototype.date() Invalid Date value = ' + value);
+			return this;
+		}
+	}
+	if (typeof(value) === 'object' && value.constructor.name === 'Date') {
+		inDate = value;
+	} else {
+		return this;
+	}
+	this.curDate = inDate;
+	this.refreshView('days');
 	return this;
 }
 
